0e21e2fd787c93143e6c3b525efe2a3eee2f7f7d
Lara Santos <106592394+LaraSantosMadeTech@users.noreply.github.com>
2022-08-01 16:15:24 +0100

    add landing page (#8)
    Co-authored-by: James Coker <james.coker@madetech.com>

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -4,3 +4,16 @@ describe('empty spec', () => {
     cy.contains('GOV.UK')
   })
 })
+
+describe('landing page', () => {
+  it('displays landing page content', () => {
+    cy.visit('http://localhost:3000')
+    cy.contains('My council account')
+    cy.contains('Blue badge application')
+  })
+  it('my council account is a clickable link', () => {
+    cy.visit('http://localhost:3000')
+    cy.contains('My council account').click()
+    cy.url().should('include', '/my-council-account')
+  })
+})
---
+++ cypress/e2e/spec.md
@@ -0,0 +1,8 @@
+# Landing Page Spec
+
+## Unit tests
+- it contains correct content
+- it (my council account) is clickable
+- it links to the right page
+- it changes on hover (the way it looks)
+- all the 'links' behave in the same way
