8dca4656eb6a1553d4c0e2117808dbf390ee2493
Sabrina <sabrina.samuel@madetech.com>
2022-08-01 17:22:53 +0100

    [Title goes here]
    Summary:
    
    Tasks:

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,19 +1,26 @@
-describe('empty spec', () => {
-  it('passes', () => {
-    cy.visit('http://localhost:3000')
-    cy.contains('GOV.UK')
-  })
-})
+describe("empty spec", () => {
+  it("passes", () => {
+    cy.visit("http://localhost:3000");
+    cy.contains("GOV.UK");
+  });
+});
 
-describe('landing page', () => {
-  it('displays landing page content', () => {
-    cy.visit('http://localhost:3000')
-    cy.contains('My council account')
-    cy.contains('Blue badge application')
-  })
-  it('my council account is a clickable link', () => {
-    cy.visit('http://localhost:3000')
-    cy.contains('My council account').click()
-    cy.url().should('include', '/my-council-account')
-  })
-})
+describe("landing page", () => {
+  it("displays landing page content", () => {
+    cy.visit("http://localhost:3000");
+    cy.contains("My council account");
+    cy.contains("Blue badge application");
+  });
+  it("my council account is a clickable link", () => {
+    cy.visit("http://localhost:3000");
+    cy.contains("My council account").click();
+    cy.url().should("include", "/my-council-account");
+  });
+});
+
+describe("my council account page", () => {
+  it("displays the page title", () => {
+    cy.visit("http://localhost:3000/my-council-account");
+    cy.contains("Creating a My Council Account");
+  });
+});
