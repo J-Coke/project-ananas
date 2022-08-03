fc296cb23f09a8856fe2e3eaa4138527fd25bffb
Sabrina <sabrina.samuel@madetech.com>
2022-08-03 14:05:27 +0100

    Update class on paragraph
    Summary:
    
    Tasks:

--- cypress/e2e/create-password.cy.js
+++ cypress/e2e/create-password.cy.js
@@ -6,4 +6,21 @@ describe("create password page", () => {
     cy.visit("/create-password");
     cy.get("h1").should("have.text", "Create a password for your account");
   });
+  it("shows the password length requirements", () => {
+    cy.visit("/create-password");
+    cy.contains("must contain 8 digits");
+  });
+  //   it("shows the password input field", () => {
+  //     cy.visit("/create-password");
+  //     cy.get("input").expect().exist();
+  //   });
 });
+
+//tests to write
+//first password field and value
+//Validate it matches requirement
+//requirement: 8 digits, at least one number and one special character
+//second password field and value
+//both values match
+//error if doesn't match
+//continue button and it goes to next page
