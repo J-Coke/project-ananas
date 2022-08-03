4d76e7f336d3713ce74326e09e8b4dfdc23c6335
Lara Santos <lara.santos@madetech.com>
2022-08-03 14:30:09 +0100

    add button, service name and more tests

--- cypress/e2e/registration-listspec.cy.js
+++ cypress/e2e/registration-listspec.cy.js
@@ -7,12 +7,26 @@ describe('empty spec', () => {
 describe('registration list page', () => {
   it('displays the title', () => {
     cy
-    .visit('http://localhost:3000/registration-list.html')
+    .visit('/public/registration-list.html')
     .contains('A list of things you will need to complete the registration')
   })
   it('displays first element of unordered list', () => {
     cy
-    .visit('http://localhost:3000/registration-list.html')
+    .visit('/public/registration-list.html')
     .get('ul li:first')
+  })
+  it('displays last element of unordered list', () => {
+    cy
+    .visit('/public/registration-list.html')
+    .get('ul li:last')
+  })
+})
+
+describe('continue button', () => {
+  it('is a clickable button to continue to registration', () => {
+    cy
+    .visit('/public/registration-list.html')
+    .get("button").contains("Continue")
+    .url().should("include", "/#")
   })
 })
