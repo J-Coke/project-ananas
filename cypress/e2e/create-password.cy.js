1776b41e536b1996a09194b281e279c17e658614
Sabrina <sabrina.samuel@madetech.com>
2022-08-03 12:38:14 +0100

    Add page title and test
    Summary:
    
    Tasks:

--- cypress/e2e/create-password.cy.js
+++ cypress/e2e/create-password.cy.js
@@ -2,4 +2,8 @@ describe("create password page", () => {
   it("loads the page", () => {
     cy.visit("/create-password");
   });
+  it("shows the correct page title", () => {
+    cy.visit("/create-password");
+    cy.get("h1").should("have.text", "Create a password for your account");
+  });
 });
