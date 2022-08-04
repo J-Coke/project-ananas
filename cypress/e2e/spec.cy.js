80bddc8ad53a9527b57de8c89b63d206dc8b53b5
Miroslav Palka <Porim@users.noreply.github.com>
2022-08-04 15:56:39 +0100

    Ft dob (#9)
    * Create a date of birth page with some basic tests
    
    Summary: ^^^
    
    Tasks:
    
    * forgoten tests...
    
    * add some new lines
    
    * fix bug with header
    
    * remove cancel button and add back button

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -11,9 +11,24 @@ describe('landing page', () => {
     cy.contains('My council account')
     cy.contains('Blue badge application')
   })
+
   it('my council account is a clickable link', () => {
     cy.visit('http://localhost:3000')
     cy.contains('My council account').click()
     cy.url().should('include', '/my-council-account')
+  })
+})
+
+describe('date of birth', () => {
+  it('displays dob page', () => {
+    cy.visit('http://localhost:3000/date-of-birth.html')
+    cy.contains('What is your date of birth?')
+    cy.contains('Month')
+  })
+  
+  it('Continue button is a clickable link', () => {
+    cy.visit('http://localhost:3000/date-of-birth.html')
+    cy.contains('Continue').click()
+    cy.url().should('include', '/email-address.html')
   })
 })
