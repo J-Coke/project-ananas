03b71a8d8a780ffc55f502c6b6e36e2537085e48
Lara Santos <lara.santos@madetech.com>
2022-08-05 14:29:21 +0100

    Remove .html from links in tests
    Summary:
    
    Tasks:

--- cypress/e2e/registration-listspec.cy.js
+++ cypress/e2e/registration-listspec.cy.js
@@ -1,23 +1,23 @@
 describe('empty spec', () => {
   it('passes', () => {
-    cy.visit('http://localhost:3000/registration-list.html')
+    cy.visit('http://localhost:3000/registration-list')
   })
 })
 
 describe('registration list page', () => {
   it('displays the title', () => {
     cy
-    .visit('http://localhost:3000/registration-list.html')
+    .visit('http://localhost:3000/registration-list')
     .contains('A list of things you will need to complete the registration')
   })
   it('displays first element of unordered list', () => {
     cy
-    .visit('http://localhost:3000/registration-list.html')
+    .visit('http://localhost:3000/registration-list')
     .get('ul li:first')
   })
   it('displays last element of unordered list', () => {
     cy
-    .visit('http://localhost:3000/registration-list.html')
+    .visit('http://localhost:3000/registration-list')
     .get('ul li:last')
   })
 })
@@ -25,7 +25,7 @@ describe('registration list page', () => {
 describe('continue button', () => {
   it('is a clickable button to continue to registration', () => {
     cy
-    .visit('http://localhost:3000/registration-list.html')
+    .visit('http://localhost:3000/registration-list')
     .get("button").contains("Continue").click()
     .url().should("include", "/name-page")
   })
