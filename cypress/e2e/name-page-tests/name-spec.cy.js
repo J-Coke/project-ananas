53d9c3865c1bda20606e86c147d01e048a0bb372
Josephine <jobeng27@gmail.com>
2022-08-08 11:16:48 +0100

    Create 'Name Page' (#18)
    Summary: This is the page where the users enter their first and last
    names. I have tested the buttons, inputs and labels.
    
    Tasks: The buttons need to be linked to the real pages; for now they are
    just hashes (`#`)

---
+++ cypress/e2e/name-page-tests/name-spec.cy.js
@@ -0,0 +1,83 @@
+describe("name page", () => {
+  before(() => {
+    cy.visit("/name-page");
+  });
+
+  context("back link", () => {
+    beforeEach(() => {
+      cy.get('[data-testid="back-link"]').as("button");
+    });
+
+    it("displays", () => {
+      cy.get("@button").should("be.visible");
+    });
+
+    it("says 'Back'", () => {
+      cy.contains("Back");
+    });
+
+    it("links to the right page", () => {
+      cy.get("@button").click();
+      cy.url().should("include", "#");
+    });
+  });
+
+  context("first name field", () => {
+    beforeEach(() => {
+      cy.get('[data-testid="first-name-field"]').as("field");
+      cy.get('[data-testid="first-name-label"]').as("label");
+    });
+
+    it("displays", () => {
+      cy.get("@field").should("be.visible");
+      cy.get("@label").should("be.visible");
+    });
+
+    it("has a label saying 'First name'", () => {
+      cy.get("@label").contains("First name");
+    });
+  });
+
+  context("surname field", () => {
+    beforeEach(() => {
+      cy.get('[data-testid="surname-field"]').as("field");
+      cy.get('[data-testid="surname-label"]').as("label");
+    });
+
+    it("displays", () => {
+      cy.get("@field").should("be.visible");
+      cy.get("@label").should("be.visible");
+    });
+
+    it("has a label saying 'Surname'", () => {
+      cy.get("@label").contains("Surname");
+    });
+  });
+
+  context("continue button", () => {
+    beforeEach(() => {
+      cy.get('[data-testid="continue-button"]').as("button");
+    });
+
+    it("displays", () => {
+      cy.get("@button").should("be.visible");
+    });
+
+    it("says 'Continue'", () => {
+      cy.contains("Continue");
+    });
+
+    it("has a green background", () => {
+      cy.get("@button").should(
+        "have.css",
+        "background-color",
+        "rgb(0, 112, 60)"
+      );
+    });
+
+    it("links to the right page", () => {
+      cy.get("@button").click();
+      cy.url().should("include", "#");
+    });
+  });
+});
