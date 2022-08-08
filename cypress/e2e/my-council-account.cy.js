031d42e86dcd1b9173a05dde18e8454092fd52b3
Sabrina <sabrina.samuel@madetech.com>
2022-08-08 17:35:21 +0100

    Create my council account (#10)
    Summary: Add components and tests to my council account page
    
    Tasks: Add correct url link for next page

---
+++ cypress/e2e/my-council-account.cy.js
@@ -0,0 +1,35 @@
+describe("my council account page", () => {
+  beforeEach(() => {
+    cy.visit("/my-council-account");
+  });
+
+  it("displays the page title", () => {
+    cy.get('[data-cy="section-heading-h1"]').contains(
+      "Creating a My Council Account"
+    );
+  });
+
+  it("can go back to the previous page", () => {
+    cy.get('[data-cy="back-link"]').contains("Back").click();
+    cy.url().should("eq", "http://localhost:3000/");
+  });
+
+  it("shows a bullet list", () => {
+    cy.get('[data-cy="bullet-list"]').contains(
+      "Photo IDs (passport, driver's licence)"
+    );
+  });
+
+  it("shows the login button", () => {
+    cy.get('[data-cy="login-btn"]');
+  });
+
+  it("shows the register button", () => {
+    cy.get('[data-cy="register-btn"]');
+  });
+
+  it("is a clickable button to register an account", () => {
+    cy.get('[data-cy="register-btn"]').click();
+    cy.url().should("include", "/#");
+  });
+});
--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,34 +1,34 @@
-describe('empty spec', () => {
-  it('passes', () => {
-    cy.visit('http://localhost:3000')
-    cy.contains('GOV.UK')
-  })
-})
+describe("empty spec", () => {
+  it("passes", () => {
+    cy.visit("http://localhost:3000");
+    cy.contains("GOV.UK");
+  });
+});
 
-describe('landing page', () => {
-  it('displays landing page content', () => {
-    cy.visit('http://localhost:3000')
-    cy.contains('My council account')
-    cy.contains('Blue badge application')
-  })
+describe("landing page", () => {
+  it("displays landing page content", () => {
+    cy.visit("http://localhost:3000");
+    cy.contains("My council account");
+    cy.contains("Blue badge application");
+  });
 
-  it('my council account is a clickable link', () => {
-    cy.visit('http://localhost:3000')
-    cy.contains('My council account').click()
-    cy.url().should('include', '/my-council-account')
-  })
-})
+  it("my council account is a clickable link", () => {
+    cy.visit("http://localhost:3000");
+    cy.contains("My council account").click();
+    cy.url().should("include", "/my-council-account");
+  });
+});
 
-describe('date of birth', () => {
-  it('displays dob page', () => {
-    cy.visit('http://localhost:3000/date-of-birth.html')
-    cy.contains('What is your date of birth?')
-    cy.contains('Month')
-  })
-  
-  it('Continue button is a clickable link', () => {
-    cy.visit('http://localhost:3000/date-of-birth.html')
-    cy.contains('Continue').click()
-    cy.url().should('include', '/email-address.html')
-  })
-})
+describe("date of birth", () => {
+  it("displays dob page", () => {
+    cy.visit("http://localhost:3000/date-of-birth.html");
+    cy.contains("What is your date of birth?");
+    cy.contains("Month");
+  });
+
+  it("Continue button is a clickable link", () => {
+    cy.visit("http://localhost:3000/date-of-birth.html");
+    cy.contains("Continue").click();
+    cy.url().should("include", "/email-address.html");
+  });
+});
