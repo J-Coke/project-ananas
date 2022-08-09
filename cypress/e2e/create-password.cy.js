4f4089d38d16e366a32540b749f883627a118ed6
Sabrina <sabrina.samuel@madetech.com>
2022-08-09 11:39:19 +0100

    Add password length validation and error

--- cypress/e2e/create-password.cy.js
+++ cypress/e2e/create-password.cy.js
@@ -1,54 +1,85 @@
 describe("create password page", () => {
-  it("loads the page", () => {
+  beforeEach(() => {
     cy.visit("/create-password");
   });
 
   it("shows the correct page title", () => {
-    cy.visit("/create-password");
-    cy.get("h1").contains("Create a password for your account");
+    cy.get('[data-cy="section-heading-h1"]').contains(
+      "Create a password for your account"
+    );
   });
 
   it("can go back to the previous page", () => {
-    cy.visit("/create-password");
-    cy.get("a").contains("Back").click();
+    cy.get('[data-cy="back-link"]').click();
     cy.url().should("eq", "http://localhost:3000/create-password/#");
   });
 
   it("shows the password length requirements", () => {
-    cy.visit("/create-password");
     cy.contains("must contain 8 digits");
   });
 
   it("shows the password input field and value", () => {
-    cy.visit("/create-password");
-    cy.get("#password")
-      .type("password test")
-      .should("have.value", "password test");
+    cy.get('[data-cy="password-input"]')
+      .type("passwordtest")
+      .should("have.value", "passwordtest");
   });
 
   it("shows the label for the confirm password input", () => {
-    cy.visit("/create-password");
     cy.contains("Confirm password");
   });
 
   it("shows the password input field and value", () => {
-    cy.visit("/create-password");
-    cy.get("#confirm-password")
-      .type("confirm password test")
-      .should("have.value", "confirm password test");
+    cy.get('[data-cy="confirm-password-input"]')
+      .type("confirmpasswordtest")
+      .should("have.value", "confirmpasswordtest");
   });
 
   it("shows the continue button", () => {
-    cy.visit("/create-password");
-    cy.get("button").contains("Continue");
+    cy.get('[data-cy="continue-button"]').contains("Continue");
   });
+
+  it("show an error if the password is less than 8 characters", () => {
+    cy.get('[data-cy="password-input"]').type("pass");
+    cy.get('[data-cy="continue-button"]').contains("Continue").click();
+    cy.contains("Enter a password with a minimum of 8 characters");
+    cy.get('[data-cy="event-name-error"]').should(
+      "have.css",
+      "color",
+      "rgb(212, 53, 28)"
+    );
+  });
+
+  // it("shows error if password doesn't contain at least one number and special character", () => {
+  //   cy.get('[data-cy="password-input"]').type("password");
+  //   cy.get('[data-cy="continue-button"]').contains("Continue").click();
+  //   cy.contains(
+  //     "Enter a password with at least one number and one special character"
+  //   );
+  //   cy.get('[data-cy="event-name-error"]').should(
+  //     "have.css",
+  //     "color",
+  //     "rgb(212, 53, 28)"
+  //   );
+  // });
+  // it("shows an error when both passwords don't match", () => {
+  //   cy.get('[data-cy="password-input"]').type("Type!1day");
+  //   cy.get('[data-cy="confirm-password-input"]').type("Type!tttt");
+  //   cy.get('[data-cy="continue-button"]').contains("Continue").click();
+  //   cy.get('[data-cy="no-match-error"]').contains(
+  //     "Both passwords should match"
+  //   );
+  // });
 });
 
 //tests to write
-//first password field and value
+
 //Validate it matches requirement
+// order of validation
+//length of password
+//type of characters required
+// passwords match
 //requirement: 8 digits, at least one number and one special character
-//second password field and value
+
 //hide both passwords
 //both values match
 //error if doesn't match
---
+++ public/javascripts/create-password.js
@@ -0,0 +1,75 @@
+const continueEvent = document.querySelector("#continue-button");
+const passwordFields = document.querySelector("#password-fields");
+// const lengthErrorPasswordFields = document.querySelector(
+//   "#length-error-password-fields"
+// );
+
+continueEvent.addEventListener("click", checkPassword);
+
+let passwordMinimumLength = 8;
+
+// console.log(passwordInput);
+
+// function showPasswordLengthError(password) {
+//   if (password.length < passwordMinimumLength) {
+//     console.log("nope");
+//   } else {
+//     console.log("ok");
+//   }
+// }
+
+// function removeClass(classToHide) {
+//   classToHide.classList.remove("no-display");
+// }
+
+// function addClass(classToAdd) {
+//   classToAdd.classList.add("no-display");
+// }
+
+function checkPassword(event) {
+  event.preventDefault();
+  let passwordInput = document.querySelector("#password");
+  // let confirmPasswordInput = document.querySelector("#confirm-password");
+  console.log(passwordInput.value.length);
+  if (passwordInput.value.length < passwordMinimumLength) {
+    passwordFields.innerHTML = `<div id="password-fields">
+        <div class="govuk-form-group govuk-form-group--error">
+          <h1 class="govuk-label-wrapper">
+            <label class="govuk-label govuk-label--l" for="event-name">
+              Create a password for your account
+            </label>
+          </h1>
+          <div id="event-name-hint" class="govuk-hint">
+            Password must contain 8 digits, at least one number and one special
+            character
+          </div>
+          <p id="event-name-error" class="govuk-error-message" data-cy="event-name-error">
+            <span class="govuk-visually-hidden" >Error:</span> Enter a password
+            with a minimum of 8 characters
+          </p>
+          <input
+            class="govuk-input govuk-input--width-20 govuk-input--error"
+            id="event-name"
+            name="event-name"
+            type="text"
+            data-cy="length-error-password-input"
+            aria-describedby="event-name-hint event-name-error"
+          />
+          <br />
+          <br />
+          <label class="govuk-label" for="confirm-password">
+            Confirm password
+          </label>
+          <input
+            class="govuk-input govuk-input--width-20"
+            id="confirm-password"
+            name="confirm-password"
+            type="text"
+            data-cy="length-error-confirm-password-input"
+          />
+        </div>
+      </div>`;
+    // removeClass(lengthErrorPasswordFields);
+    // addClass(initialPasswordFields);
+  }
+}
